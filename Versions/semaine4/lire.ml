31233efa39952edd85e47c7ecdfbde971dc9b19d
ElemusHope <z13933063909@gmail.com>
2020-02-03 17:11:21 +0100

    第二版完成

---
+++ Versions/semaine4/lire.ml
@@ -0,0 +1,39 @@
+let lire fichier =
+  let ic = open_in fichier in
+  let line = input_line ic in
+  close_in ic;
+  line;;
+
+let explode s =
+  let rec exp i l =
+    if i < 0 then l 
+    else exp (i - 1) ((int_of_char(s.[i])-48)::l) in 
+
+  exp (String.length s-1) [];;
+
+
+let rec take n l =
+  if n = 0 then [] else
+    match l with 
+      h::t -> h:: take (n-1) t
+;;
+
+let rec drop n l =
+  if n=0 then l else
+    match l with
+      h::t -> drop (n-1) t
+;;
+
+   
+let rec split chunksize l=
+  try 
+    take chunksize l::
+    split chunksize (drop chunksize l)
+  with
+    _ -> match l with [] -> [] | _ -> [l]
+;;
+
+
+
+
+
